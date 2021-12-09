fb28dbd38b8be371ed98a95544d366fbf4652af7
JObog <jobogbaimhe@gmail.com>
2021-12-09 12:30:40 -0500

    using tailwind cdn for quicker styling

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const b = document.querySelector(".bitset");
+window.onload = () => {
+  const bs = document.querySelector(".bitset");
 
-for (let i = 0; i < 64; i++) {
-  const div = document.createElement("div");
-  div.className = `bit ${
-    i == 0 ? "sign" : i >= 1 && i <= 38 ? "exponent" : "mantissa"
-  }`;
-  div.innerText = "1";
-  b.appendChild(div);
-}
+  for (let i = 0; i < 64; i++) {
+    const div = document.createElement("div");
+    div.className = `text-4xl font-bold border border-gray-300 bg-red-100 px-0.45`;
+    div.innerText = "0";
+    bs.appendChild(div);
+  }
+};
