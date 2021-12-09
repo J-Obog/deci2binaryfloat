1d2b247af3ce0bbc12e59600334eda4cd0d226a1
JObog <jobogbaimhe@gmail.com>
2021-12-09 13:43:37 -0500

    adding prettier config

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-window.onload = () => {
-  const bs = document.querySelector(".bitset");
+const bs = document.querySelector('.bitset');
 
-  for (let i = 0; i < 64; i++) {
-    const div = document.createElement("div");
-    div.className = `text-4xl font-bold border border-gray-300 bg-red-100 px-0.45`;
-    div.innerText = "0";
-    bs.appendChild(div);
-  }
+window.onload = () => {
+    for (let i = 0; i < 64; i++) {
+        const div = document.createElement('div');
+        let clr = i == 0 ? 'blue' : i >= 1 && i <= 11 ? 'green' : 'red';
+        div.className = `text-4xl font-bold border-black bg-${clr}-200 px-1`;
+        div.innerText = '0';
+        bs.appendChild(div);
+    }
 };
